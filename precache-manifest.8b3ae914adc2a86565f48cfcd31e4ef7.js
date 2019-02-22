beac6924f74e43a9a4bc2ed37176a7bbb0280f76
ryanAltobello <rchap504@gmail.com>
2019-02-21 18:08:16 -0600

    Updates

---
+++ precache-manifest.8b3ae914adc2a86565f48cfcd31e4ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1981d043930fe484638270610752fdf1",
+    "url": "/hacker-news-clone/static/media/logo.1981d043.jpg"
+  },
+  {
+    "revision": "f58bbedcefda0cc29c5a",
+    "url": "/hacker-news-clone/static/js/runtime~main.f58bbedc.js"
+  },
+  {
+    "revision": "9b616fcfe055ce43fea7",
+    "url": "/hacker-news-clone/static/js/main.9b616fcf.chunk.js"
+  },
+  {
+    "revision": "3a635dea6288700d5ce2",
+    "url": "/hacker-news-clone/static/js/2.3a635dea.chunk.js"
+  },
+  {
+    "revision": "9b616fcfe055ce43fea7",
+    "url": "/hacker-news-clone/static/css/main.09d00f15.chunk.css"
+  },
+  {
+    "revision": "138971a763678d351675a01a3768d929",
+    "url": "/hacker-news-clone/index.html"
+  }
+];
